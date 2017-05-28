69dda4798400ea7785241b59bfb685e738dd6df0
Adam Baniewicz <adambaniewicz@gmail.com>
2017-05-28 14:49:35 +0200

    hamming: better solution

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,13 +1,10 @@
 class Hamming
   def self.compute(dna1, dna2)
     raise ArgumentError.new("Strands need to be the same length") unless dna1.length == dna2.length
-
-    counter = 0
-    dna1.chars.zip(dna2.chars) { |a| counter += a.uniq.count - 1 }
-    counter
+    dna1.chars.zip(dna2.chars).count { |a, b| a != b }
   end
 end
 
 module BookKeeping
-   VERSION = 3 # Where the version number matches the one in the test.
+   VERSION = 3
  end
